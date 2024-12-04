c5b8056146a7e9a21b85c1aa6791c6b9d96bd44e
Jarvis <jarvwashington@gmail.com>
2024-12-04 17:54:32 -0600

    adjusted Navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -18,16 +18,17 @@ return (
               <Nav.Link as={Link} to='/projects'>
                 Projects
               </Nav.Link>
-              <Nav.Link as={Link} to='/resume'>
+            </Nav>
+            <Nav className='ml-auto d-flex'>
+            <Nav.Link as={Link} to='/resume'>
                 Resume
               </Nav.Link>
-              
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
       <div>
-        
+
       </div>
     </>
   );
